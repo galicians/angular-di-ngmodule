f293c16e3ffa7141de66c1d16f46259c1dca7c40
galicians <galicians@gmail.com>
2020-05-12 09:07:35 +0100

    1 file modified

--- src/app/analytics-demo.interface.ts
+++ src/app/analytics-demo.interface.ts
@@ -2,6 +2,10 @@ export interface AnalyticsImplementation {
   recordEvent(metric: Metric): void;
 }
 
+export interface Metric {
+  eventName: string;
+  scope: string;
+}
 let metric: Metric = {
   eventName: "loggedIn",
   scope: "pablo"
