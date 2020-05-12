2ca475873963b128bf2bf2a5f35265cf433ff500
galicians <galicians@gmail.com>
2020-05-12 09:12:01 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { UserService } from './user.service';
+import { Metric, AnalyticsImplementation } from './analytics-demo.interface';
+import { AnalyticsService } from './analytics.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  providers: [ UserService ],
+  providers: [ UserService,{ provide: AnalyticsService, useFactory() { const loggingImplementation: AnalyticsImplementation = {
+    recordEvent: (metric: Metric): void => {
+      console.log('The metric is :', metric)
+    }
+  }}} ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
